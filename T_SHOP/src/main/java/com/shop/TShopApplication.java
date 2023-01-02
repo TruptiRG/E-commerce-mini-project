34bde11a8cbc73c575cd47ce520cd11fd2b23706
TruptiRG <trupti16ghadekar@gmail.com>
2023-01-02 17:41:58 +0530

    models

---
+++ T_SHOP/src/main/java/com/shop/TShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TShopApplication.class, args);
+	}
+
+}
